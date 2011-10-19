801a31b58dac222aaa234dd53f2be950b4576b4e
Phil Hagelberg <technomancy@gmail.com>
2011-10-18 21:45:51 -0700

    Add config notes to start.sh

--- start.sh
+++ start.sh
@@ -1,9 +1,21 @@
 #!/bin/bash
 
+# Set it to use the C build pack.
+
+# Run: ./configure && make && ./znc --makepass # copy password for below
+# $ heroku config:add NICK=mynick PASS=sha256#05ef1effb902c58d97ad04a7f869c62f8d6d9ac4a3ac093f548e9eece7a8efec#mr)F/l.Z_y._Jjpyn7Gi#
+
+# Deploy!
+# $ heroku ps:scale znc=1
+
+# Also need access to the TCP router (currently in private beta) to use this:
+# $ heroku routes:create
+# $ heroku routes:attach tcp://route.heroku.com:$PORT  znc.1
+
 set -e
 
-if [ "$PORT" = "" ] && [ "$PASS" = "" ]; then
-  echo "! Need $PORT and $PASS set!"
+if [ "$PORT" = "" ] || [ "$PASS" = "" ] || [ "$NICK" = "" ]; then
+  echo "! Need $PORT, $NICK, and $PASS set!"
   exit 1
 fi
 
